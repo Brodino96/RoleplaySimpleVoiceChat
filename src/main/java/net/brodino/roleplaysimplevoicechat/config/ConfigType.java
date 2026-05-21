c9df49f01713f47df7e392a388379c574d6dcf9b
Brodino <brodino96@gmail.com>
2026-05-22 01:52:25 +0200

    feat(config): updated default configs to match current ElysiumCraft values

--- src/main/java/net/brodino/roleplaysimplevoicechat/config/ConfigType.java
+++ src/main/java/net/brodino/roleplaysimplevoicechat/config/ConfigType.java
@@ -2,9 +2,9 @@ package net.brodino.roleplaysimplevoicechat.config;
 
 public class ConfigType {
     public float whisperDistance = 6.0F;
-    public float normalDistance = 24.0F;
-    public float shoutDistance = 48.0F;
-    public float voiceExtenderDistance = 128.0F;
+    public float normalDistance = 12.0F;
+    public float shoutDistance = 24.0F;
+    public float voiceExtenderDistance = 48.0F;
 
     public float getWhisperDistance() { return this.whisperDistance; }
     public float getNormalDistance() { return this.normalDistance; }
