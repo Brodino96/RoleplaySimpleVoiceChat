eea7b27e53ee80ef750a08652e1cea32fb47a0ab
Brodino <brodino96@gmail.com>
2026-05-22 13:37:41 +0200

    refactor(commands): simplified structure of command declaration to take into account possible future commands

--- src/main/java/net/brodino/roleplaysimplevoicechat/commands/CommandHandler.java
+++ src/main/java/net/brodino/roleplaysimplevoicechat/commands/CommandHandler.java
@@ -1,18 +1,22 @@
 package net.brodino.roleplaysimplevoicechat.commands;
 
-import net.brodino.roleplaysimplevoicechat.RoleplaySimpleVoicechat;
+import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
 
 public class CommandHandler {
 
     public static void initialize() {
-        ServerLifecycleEvents.START_DATA_PACK_RELOAD.register((server, resManager) -> {
-            RoleplaySimpleVoicechat.reloadConfig();
-        });
-
         CommandRegistrationCallback.EVENT.register((dispatcher, ra, e) -> {
-            ReloadConfigCommand.register(dispatcher);
+            registerCommand(dispatcher);
         });
+    }
+
+    private static void registerCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("rpsvc")
+            .requires(src -> src.hasPermissionLevel(2))
+            .then(ReloadConfigCommand.getCommand())
+        );
     }
 }
