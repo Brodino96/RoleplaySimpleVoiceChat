1559f15da51375fe8289b4918b49604fe0a83ea2
Brodino <brodino96@gmail.com>
2026-05-22 14:37:56 +0200

    feat(death): dead players can't talk in voicechat

--- src/main/java/net/brodino/roleplaysimplevoicechat/client/mixin/HudRenderMixin.java
+++ src/main/java/net/brodino/roleplaysimplevoicechat/client/mixin/HudRenderMixin.java
@@ -10,7 +10,9 @@ import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,12 +21,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = RenderEvents.class, remap = false)
 public class HudRenderMixin {
 
+    @Shadow
+    @Final
+    private MinecraftClient minecraft;
     @Unique
     private static final Identifier SPEAKER_OFF_ICON = new Identifier("voicechat", "textures/icons/speaker_off.png");
 
     @Inject(method = "onRenderHUD", at = @At("HEAD"), cancellable = true)
     private void onRenderHUD(MatrixStack stack, float tickDelta, CallbackInfo ci) {
         if (VoicechatClient.CLIENT_CONFIG.hideIcons.get() || !VoicechatClient.CLIENT_CONFIG.showHudIcons.get()) {
+            ci.cancel(); // Doesn't render anything
+            return;
+        }
+
+        if (this.minecraft.player != null && this.minecraft.player.isDead()) {
+            this.renderIcon(stack, SPEAKER_OFF_ICON);
             ci.cancel();
             return;
         }
